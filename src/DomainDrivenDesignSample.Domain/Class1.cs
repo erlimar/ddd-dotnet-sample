f3c97cb0263729239e3c67ee928527796bc2e33a
Erlimar Silva Campos <erlimar@gmail.com>
2023-08-11 14:58:22 -0300

    Cria projeto inicial com biblioteca de domínio

---
+++ src/DomainDrivenDesignSample.Domain/Class1.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) Time DomainDrivenDesignSample. Todos os direitos reservados.
+// Este arquivo é parte do projeto DomainDrivenDesignSample.
+
+namespace DomainDrivenDesignSample.Domain;
+
+public class Class1
+{
+
+}
