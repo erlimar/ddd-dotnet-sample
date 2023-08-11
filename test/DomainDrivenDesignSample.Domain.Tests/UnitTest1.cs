f3f1938d80795693aa3c5f1e7abe291906d76753
Erlimar Silva Campos <erlimar@gmail.com>
2023-08-11 19:01:38 -0300

    Cria projeto de testes

---
+++ test/DomainDrivenDesignSample.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+// Copyright (c) Time DomainDrivenDesignSample. Todos os direitos reservados.
+// Este arquivo é parte do projeto DomainDrivenDesignSample.
+
+namespace DomainDrivenDesignSample.Domain.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
